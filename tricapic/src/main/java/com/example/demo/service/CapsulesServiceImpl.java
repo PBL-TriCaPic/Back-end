824a8213d3bce6d8af48dcdd4929e2d68bab9d67
SaetoIto <saetohack@gmail.com>
2023-10-24 19:35:21 +0900

    カプセル作成

--- tricapic/src/main/java/com/example/demo/service/CapsulesServiceImpl.java
+++ tricapic/src/main/java/com/example/demo/service/CapsulesServiceImpl.java
@@ -1,30 +1,25 @@
-package com.example.demo.Service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.data_tables.Capsules;
 
 @Service
-public class DatabaseService {
-    
-    //コンストラクタインジェクション
-    private CompanionRepo companionRepo;
+public class CapsulesServiceImpl implements CapsulesService {
+
+    private final CapsulesRepository capsulesRepository;
 
     @Autowired
-    public DatabaseService(CompanionRepo companionRepo) {
-
-        this.companionRepo = companionRepo;
+    public CapsulesServiceImpl(CapsulesRepository capsulesRepository) {
+        this.capsulesRepository = capsulesRepository;
     }
 
-    //追加メゾット一覧
-    //Companion Table
-    public Long createCapsule(Capsules companion) {
-        try{
-            Capsules c = companionRepo.save(companion);
-            return c.getCompanionId();
-        }catch(Exception e){
-            return null;
-        }
+    @Override
+    public Capsules createCapsule(Capsules capsule) {
+        return capsulesRepository.save(capsule);
+    }
+
+    @Override
+    public Capsules getCapsule(Long id) {
+        return capsulesRepository.findById(id).orElse(null);
     }
 }
