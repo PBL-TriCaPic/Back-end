996f31507df01de4bde8f3313788f4330aaa2782
SaetoIto <saetohack@gmail.com>
2023-11-04 11:42:03 +0900

    add code for storing metadata to DB

--- tricapic/src/main/java/com/example/demo/service/CapsulesService.java
+++ tricapic/src/main/java/com/example/demo/service/CapsulesService.java
@@ -1,12 +1,19 @@
 package com.example.demo.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.Entity.Image;
 import com.example.demo.data_interfaces.CapsulesRepo;
 import com.example.demo.data_interfaces.ImageRepo;
 import com.example.demo.data_tables.Capsules;
+import com.example.demo.data_tables.Photos;
 
 @Service
 public class CapsulesService {
@@ -16,6 +23,7 @@ public class CapsulesService {
     
     public CapsulesService(CapsulesRepo capsulesRepo) {
         this.capsulesRepo = capsulesRepo;
+        this.imageRepo = imageRepo; // ImageRepoを初期化
     }
 
     // create capsules
@@ -33,15 +41,25 @@ public class CapsulesService {
         return capsulesRepo.findById(id).isPresent();
     }
 
-     // save image
-     public boolean saveImage(byte[] imageData) {
+    // save image
+    public String saveImage(MultipartFile file) {
         try {
-            Image image = new Image();
-            image.setImageData(imageData);
-            ImageRepo.save(image); // imageRepo を介してデータベースに保存
-            return true;
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            String uploadPath = "your/upload/directory"; // 適切なディレクトリパスを設定
+            Path filePath = Paths.get(uploadPath).resolve(fileName);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            // ファイルのメタデータをデータベースに保存するロジックを追加
+            Photos photo = new Photos();
+            photo.setFileName(fileName);
+            photo.setFilePath(filePath.toString());
+            imageRepo.save(photo);
+
+            return fileName;
         } catch (Exception e) {
-            return false;
+            e.printStackTrace();
+            return null;
         }
     }
 }
+
