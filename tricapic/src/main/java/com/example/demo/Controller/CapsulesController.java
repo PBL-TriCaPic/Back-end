982aee695d511b6faee7eb41c0bdccc429e0f582
SaetoIto <saetohack@gmail.com>
2023-11-05 20:59:56 +0900

    save_image

--- tricapic/src/main/java/com/example/demo/Controller/CapsulesController.java
+++ tricapic/src/main/java/com/example/demo/Controller/CapsulesController.java
@@ -2,10 +2,10 @@ package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.data_tables.Capsules;
 import com.example.demo.service.CapsulesService;
@@ -31,14 +31,13 @@ public class CapsulesController {
         return capsulesService.getCapsule(id);
     }
 
-//     @PostMapping("/saveImage")
-// public ResponseEntity<String> saveImage(@RequestBody byte[] imageData) {
-//     String fileName = capsulesService.saveImage(imageData);
-//     if (fileName != null) {
-//         return new ResponseEntity<>("Image saved successfully with file name: " + fileName, HttpStatus.OK);
-//     } else {
-//         return new ResponseEntity<>("Failed to save image", HttpStatus.BAD_REQUEST);
-//     }
-// }
-
+    @PostMapping("/saveImage")
+    public ResponseEntity<String> saveImage(@RequestParam("file") MultipartFile file) {
+        String fileName = capsulesService.saveImage(file);
+        if (fileName != null) {
+            return new ResponseEntity<>("Image saved successfully with file name: " + fileName, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to save image", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- tricapic/src/main/java/com/example/demo/data_interfaces/ImageRepo.java
+++ tricapic/src/main/java/com/example/demo/data_interfaces/ImageRepo.java
@@ -1,9 +1,12 @@
 package com.example.demo.data_interfaces;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.data_tables.Photos;
+
 
 @Repository
 public interface ImageRepo extends JpaRepository<Photos, Long> {
