d79a72f6a82d9d2062e4b30f9e5d8760ab73de7f
SaetoIto <saetohack@gmail.com>
2023-11-07 21:52:38 +0900

    add

--- tricapic/src/main/java/com/example/demo/Controller/CapsulesController.java
+++ tricapic/src/main/java/com/example/demo/Controller/CapsulesController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.example.demo.data_tables.Capsules;
-import com.example.demo.service.CapsulesService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;;
 
 
 @RestController
@@ -35,9 +36,12 @@ public class CapsulesController {
     public ResponseEntity<String> saveImage(@RequestParam("file") MultipartFile file) {
         String fileName = capsulesService.saveImage(file);
         if (fileName != null) {
+            System.out.println("Image saved successfully with file name: " + fileName);
             return new ResponseEntity<>("Image saved successfully with file name: " + fileName, HttpStatus.OK);
         } else {
+            System.out.println("Failed to save image");
             return new ResponseEntity<>("Failed to save image", HttpStatus.BAD_REQUEST);
         }
     }
+
 }
--- tricapic/src/main/java/com/example/demo/service/CapsulesService.java
+++ tricapic/src/main/java/com/example/demo/service/CapsulesService.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,8 +45,13 @@ public class CapsulesService {
     public String saveImage(MultipartFile file) { //MultipartFileインスタンスを使いfileを引数とする
         try {
             String fileName = StringUtils.cleanPath(file.getOriginalFilename()); //fileからオリジナルのファイル名を取得しStringUtils.cleanPath() メソッドで安全なファイル名を取得
-            String uploadPath = "C:\\Users\\2021052\\PBL\\Image"; // 適切なディレクトリパスを設定、アップロード先のディレクトリだと思う
-            Path filePath = Paths.get(uploadPath).resolve(fileName);
+            String uploadPath = "C:\\Users\\2021052\\PBL\\Upload"; // 適切なディレクトリパスを設定、アップロード先のディレクトリだと思う
+            
+            //Path filePath = Paths.get(uploadPath).resolve(fileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+            //Path filePath = Paths.get(uploadPath, fileName.replace("/", "\\"));
+
+            
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
             // ファイルのメタデータをデータベースに保存
@@ -55,11 +59,9 @@ public class CapsulesService {
             photo.setImageData(filePath.toString()); // ファイルパスを設定
             imageRepo.save(photo);
 
-            System.out.println("ここまでは動いている");
-
             return filePath.toString(); // 保存されたファイルのフルパスを文字列で返す
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); //エラー情報をコンソールに出力
             return null;
         }
     }
