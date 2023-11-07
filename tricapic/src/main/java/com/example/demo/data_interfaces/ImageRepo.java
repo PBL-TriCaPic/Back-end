54dbc3267858377f5e0158ba8e2617a0835cfeea
SaetoIto <saetohack@gmail.com>
2023-11-07 12:02:59 +0900

    save_image

--- tricapic/src/main/java/com/example/demo/data_interfaces/ImageRepo.java
+++ tricapic/src/main/java/com/example/demo/data_interfaces/ImageRepo.java
@@ -1,7 +1,5 @@
 package com.example.demo.data_interfaces;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- tricapic/src/main/java/com/example/demo/service/CapsulesService.java
+++ tricapic/src/main/java/com/example/demo/service/CapsulesService.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
