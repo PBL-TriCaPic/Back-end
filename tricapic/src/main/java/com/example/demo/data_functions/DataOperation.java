7896ec089ca6eafd4fc4b2b39b97f358be11ab13
ryunosuke-tateyama <b1021182@fun.ac.jp>
2023-11-20 11:05:57 +0900

    createCapsule_tateyama

--- tricapic/src/main/java/com/example/demo/data_functions/DataOperation.java
+++ tricapic/src/main/java/com/example/demo/data_functions/DataOperation.java
@@ -1,20 +1,25 @@
 package com.example.demo.data_functions;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.data_interfaces.*;
-import com.example.demo.data_tables.*;
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-
-import org.apache.commons.codec.binary.Base64;
+import com.example.demo.data_interfaces.CapsulesRepo;
+import com.example.demo.data_interfaces.PhotosRepo;
+import com.example.demo.data_interfaces.UsersRepo;
+import com.example.demo.data_tables.Capsules;
+import com.example.demo.data_tables.Photos;
+import com.example.demo.data_tables.Users;
 
 
 @Service
@@ -36,7 +41,7 @@ public class DataOperation {
 
     //Add new data for Users table
     public boolean createUser(Users newUser) {
-        if (newUser.getEmail() == null || newUser.getEmail().isEmpty() 
+        if (newUser.getEmail() == null || newUser.getEmail().isEmpty()
         || newUser.getUserId() == null || newUser.getUserId().isEmpty()
         || newUser.getPassword() == null || newUser.getPassword().isEmpty()
         || newUser.getName() == null || newUser.getName().isEmpty()) {
@@ -66,6 +71,58 @@ public class DataOperation {
     public Users readUser(String email) {
         return usersRepo.findByEmail(email);
     }
+
+    // カプセルテーブルに情報追加
+    public Long createCapsule(Capsules newCapsule, String imageDataBase64) {
+    if (newCapsule.getCapsuleDate() == null
+    || newCapsule.getCapsulesId() == null
+    || Float.isNaN(newCapsule.getCapsuleLat())
+    || Float.isNaN(newCapsule.getCapsuleLon())) {
+        return null; // 必要なデータが不足している場合はnullを返す
+    }
+
+    try {
+
+        // ユーザーが存在するか確認
+        Users user = newCapsule.getUsers();
+        if (user == null || usersRepo.findById(user.getUserId()).orElse(null) == null) {
+            return null;
+        }
+
+        // カプセルの作成日時を現在時刻に設定
+        newCapsule.setCapsuleDate(LocalDateTime.now());
+
+        // 新しいカプセルをデータベースに保存
+        Capsules savedCapsule = capsulesRepo.save(newCapsule);
+
+        // 画像データをデコードしてファイルに保存
+        saveImageData(savedCapsule.getCapsulesId(), imageDataBase64);
+
+        // 保存後のカプセルIDを取得して返す
+        return savedCapsule.getCapsulesId();
+    } catch (Exception e) {
+        return null;
+    }
+}
+
+    // 画像データをデコードしてファイルに保存
+    private boolean saveImageData(Long capsulesId, String imageDataBase64) {
+        try {
+            byte[] imageData = Base64.decodeBase64(imageDataBase64);
+
+            // ファイルパスの設定
+            String filePath = "path/to/save/" + capsulesId + ".jpg";
+
+            // 画像データをファイルに保存
+            Files.write(Paths.get(filePath), imageData);
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
     // ユーザーIDに基づいてカプセル情報を取得
     public List<Capsules> getCapsulesInfoByUserId(String userId) {
