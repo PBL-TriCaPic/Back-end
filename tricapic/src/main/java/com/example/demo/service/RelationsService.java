cbbb000e972160636b3a2fb3ff68d7edd9bf23b3
ryunosuke-tateyama <b1021182@fun.ac.jp>
2023-11-28 15:23:49 +0900

    add RelationsService

---
+++ tricapic/src/main/java/com/example/demo/service/RelationsService.java
@@ -0,0 +1,108 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.data_interfaces.RelationsRepo;
+import com.example.demo.data_tables.Relations;
+import com.example.demo.data_tables.Users;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class RelationsService {
+    private final RelationsRepo relationsRepository;
+
+
+    // フォローリクエストを処理するサービスロジック
+    @Transactional
+    public boolean addFollow(Users followerUser, Users followedUser, Long id) {
+
+        Relations follow = relationsRepository.findByFollowerAndFollowed(followerUser, followedUser);
+
+        // 登録されたフォローがまだない場合
+        if (follow == null) {
+
+            follow = new Relations();
+            follow.setFollowerId(followerUser);
+            follow.setFollowedId(followedUser);
+            relationsRepository.save(follow);
+
+        } else {
+            // 登録済みフォローの場合、失敗
+            return false;
+        }
+        // フォロー登録時の成功
+        return true;
+    }
+
+
+    // アンフォローリクエストを処理するサービスロジック
+    @Transactional
+    public boolean deleteFollow(Users followerUser, Users followedUser, Long id) {
+
+        Relations follow = relationsRepository.findByFollowerAndFollowed(followerUser, followedUser);
+
+        // 該当するフォローがあれば
+        if (follow != null) {
+
+            // アンフォローする
+            relationsRepository.delete(follow);
+
+        } else {
+
+            // アンフォロー失敗
+            return false;
+        }
+        // アンフォロー成功
+        return true;
+    }
+
+    // ログインユーザーがフォローするユーザー数を計算する
+    public Long getFollowerCount(String followerUser) {
+
+        return relationsRepository.countByFollower(followerUser);
+    }
+
+    // ログインユーザーをフォローするユーザー数を計算する
+    public Long getFollowedCount(String followedUser) {
+
+        return relationsRepository.countByFollowed(followedUser);
+    }
+
+    // ログインユーザーがフォローするユーザーリスト（フォローリスト）を抽出する
+    public List<String> getFollower(String userId) {
+
+        return relationsRepository.findFollowerByUserId(userId);
+    }
+
+    // ログインユーザーをフォローするユーザーリスト（フォロワーリスト）を抽出する
+    public List<String> getFollowed(String userId) {
+
+        return relationsRepository.findFollowedByUserId(userId);
+    }
+
+    // ログインユーザーのフォローリストからアンフォローリクエストを処理する。
+    @Transactional
+    public boolean deleteFollowList(Users followerUser, Users followedUser) {
+
+        Relations follow = relationsRepository.findByFollowerAndFollowed(followerUser, followedUser);
+
+        // 該当するフォローがあれば
+        if (follow != null) {
+
+            // アンフォローする
+            relationsRepository.delete(follow);
+
+        } else {
+
+            // フォローがない場合はまだ登録されていないというメッセージを表示する
+            return false;
+        }
+        // アンフォロー成功メッセージ
+        return true;
+    }
+}
