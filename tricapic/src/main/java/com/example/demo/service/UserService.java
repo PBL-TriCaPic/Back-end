bf34a5df9ff0baa41749799e0cc67a5995ae4b4e
SaetoIto <saetohack@gmail.com>
2023-12-07 12:55:44 +0900

    without capsuleIdList

--- tricapic/src/main/java/com/example/demo/service/UserService.java
+++ tricapic/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+<<<<<<< HEAD
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,19 @@ import org.springframework.stereotype.Service;
 import com.example.demo.data_interfaces.CapsulesRepo;
 import com.example.demo.data_interfaces.UsersRepo;
 import com.example.demo.data_tables.Capsules;
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.data_interfaces.UsersRepo;
+>>>>>>> 34d1e41 (without capsuleIdList)
 import com.example.demo.data_tables.Users;
 
 @Service
 public class UserService {
 
     private final UsersRepo usersRepo;
+<<<<<<< HEAD
     private final CapsulesRepo capsulesRepo;
 
     @Autowired
@@ -32,5 +40,15 @@ public class UserService {
 
     public List<Capsules> getCapsulesByUserId(String userId) {
         return capsulesRepo.findByUsers_UserId(userId);
+=======
+
+    @Autowired
+    public UserService(UsersRepo usersRepo) {
+        this.usersRepo = usersRepo;
+    }
+
+    public Users getUserById(String userId) {
+        return usersRepo.findByUserId(userId);
+>>>>>>> 34d1e41 (without capsuleIdList)
     }
 }
