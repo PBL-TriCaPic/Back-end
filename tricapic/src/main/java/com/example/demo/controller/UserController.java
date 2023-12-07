02bbedab8e9d664cb816592a2eddf0291b6daed8
SaetoIto <saetohack@gmail.com>
2023-12-07 14:12:13 +0900

    almost completed

--- tricapic/src/main/java/com/example/demo/controller/UserController.java
+++ tricapic/src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.data_tables.Capsules;
 import com.example.demo.data_tables.Users;
+import com.example.demo.service.UserCapsuleData; // ここでUserCapsuleDataを正しくインポートする
 import com.example.demo.service.UserService;
 
 @RestController
@@ -19,7 +23,15 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}")
-    public Users getUserById(@PathVariable String userId) {
-        return userService.getUserById(userId);
+    public UserCapsuleData getUserWithCapsules(@PathVariable String userId) {
+        Users user = userService.getUserById(userId);
+        List<Capsules> capsules = userService.getCapsulesByUserId(userId);
+    
+        // UserCapsuleData クラスのインスタンスを作成し、ユーザー情報とカプセル情報を設定します
+        UserCapsuleData userCapsuleData = new UserCapsuleData(user, capsules);
+    
+        // UserCapsuleData インスタンスを返します
+        return userCapsuleData;
     }
+    
 }
--- tricapic/src/main/java/com/example/demo/service/UserService.java
+++ tricapic/src/main/java/com/example/demo/service/UserService.java
@@ -1,22 +1,32 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.data_interfaces.CapsulesRepo;
 import com.example.demo.data_interfaces.UsersRepo;
+import com.example.demo.data_tables.Capsules;
 import com.example.demo.data_tables.Users;
 
 @Service
 public class UserService {
 
     private final UsersRepo usersRepo;
+    private final CapsulesRepo capsulesRepo;
 
     @Autowired
-    public UserService(UsersRepo usersRepo) {
+    public UserService(UsersRepo usersRepo, CapsulesRepo capsulesRepo) {
         this.usersRepo = usersRepo;
+        this.capsulesRepo = capsulesRepo;
     }
 
     public Users getUserById(String userId) {
         return usersRepo.findByUserId(userId);
+    }
+
+    public List<Capsules> getCapsulesByUserId(String userId) {
+        return capsulesRepo.findByUsers_UserId(userId);
     }
 }
