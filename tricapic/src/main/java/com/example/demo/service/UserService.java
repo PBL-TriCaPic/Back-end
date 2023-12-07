d5c0c6a09976570d56c0f08c3662ab6a030faaa3
SaetoIto <saetohack@gmail.com>
2023-12-07 14:12:13 +0900

    almost completed

--- tricapic/src/main/java/com/example/demo/service/UserService.java
+++ tricapic/src/main/java/com/example/demo/service/UserService.java
@@ -1,20 +1,11 @@
 package com.example.demo.service;
 
-<<<<<<< HEAD
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.data_interfaces.CapsulesRepo;
 import com.example.demo.data_interfaces.UsersRepo;
 import com.example.demo.data_tables.Capsules;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.data_interfaces.UsersRepo;
->>>>>>> 34d1e41 (without capsuleIdList)
 import com.example.demo.data_tables.Users;
 
 @Service
@@ -41,14 +32,19 @@ public class UserService {
     public List<Capsules> getCapsulesByUserId(String userId) {
         return capsulesRepo.findByUsers_UserId(userId);
 =======
+    private final CapsulesRepo capsulesRepo;
 
     @Autowired
-    public UserService(UsersRepo usersRepo) {
+    public UserService(UsersRepo usersRepo, CapsulesRepo capsulesRepo) {
         this.usersRepo = usersRepo;
+        this.capsulesRepo = capsulesRepo;
     }
 
     public Users getUserById(String userId) {
         return usersRepo.findByUserId(userId);
->>>>>>> 34d1e41 (without capsuleIdList)
+    }
+
+    public List<Capsules> getCapsulesByUserId(String userId) {
+        return capsulesRepo.findByUsers_UserId(userId);
     }
 }
