55aa21a634ac6335a1abe4615599c68d24b7ebfc
SaetoIto <saetohack@gmail.com>
2023-12-13 16:13:22 +0900

    aploximately completed

--- tricapic/src/main/java/com/example/demo/controller/UserController.java
+++ tricapic/src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.data_tables.Capsules;
 import com.example.demo.data_tables.Users;
-import com.example.demo.service.UserCapsuleData;
 import com.example.demo.service.UserService;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 @RestController
 public class UserController {
@@ -22,16 +23,71 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users/{userId}")
-    public UserCapsuleData getUserWithCapsules(@PathVariable String userId) {
-        Users user = userService.getUserById(userId);
-        List<Capsules> capsules = userService.getCapsulesByUserId(userId);
+    @GetMapping("/users/{userId}") //エンドポイントもっと複雑に
+public CapsuleUser getUserWithCapsules(@PathVariable String userId) {
+    Users user = userService.getUserById(userId);
+    List<Capsules> capsules = userService.getCapsulesByUserId(userId);
+
+    CapsuleUser capsuleUser = new CapsuleUser();//インスタンス
+
+    capsuleUser.setIconImage(user.getIconImage());
+    capsuleUser.setProfile(user.getProfile());
+    capsuleUser.setName(user.getName());
+
+    List<String> capsuleIds = capsules.stream().map(capsule -> String.valueOf(capsule.getCapsulesId())).collect(Collectors.toList());
     
-        // UserCapsuleData クラスのインスタンスを作成し、ユーザー情報とカプセル情報を設定します
-        UserCapsuleData userCapsuleData = new UserCapsuleData(user, capsules);
-    
-        // UserCapsuleData インスタンスを返します
-        return userCapsuleData;
+    capsuleUser.setCapsulesId(capsuleIds);
+
+    return capsuleUser;
+}
+
+
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    class CapsuleUser {
+
+        private String iconImage;
+        private String profile;
+        private String name;
+        private List<String> capsulesId;
+
+            // iconImageのゲッター
+        public String getIconImage() {
+            return iconImage;
+        }
+
+        // iconImageのセッター
+        public void setIconImage(String iconImage) {
+            this.iconImage = iconImage;
+        }
+
+        // profileのゲッター
+        public String getProfile() {
+            return profile;
+        }
+
+        // profileのセッター
+        public void setProfile(String profile) {
+            this.profile = profile;
+        }
+
+        // nameのゲッター
+        public String getName() {
+            return name;
+        }
+
+        // nameのセッター
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        // capsulesIdのゲッター
+        public List<String> getCapsulesId() {
+            return capsulesId;
+        }
+
+        // capsulesIdのセッター
+        public void setCapsulesId(List<String> capsulesId) {
+            this.capsulesId = capsulesId;
+        }
     }
-    
 }
