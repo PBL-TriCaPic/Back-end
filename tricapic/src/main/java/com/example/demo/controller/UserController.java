2bd2d3e4fcf6ed413578d703371f706daa0101ff
SaetoIto <saetohack@gmail.com>
2023-12-17 21:29:22 +0900

    encode

--- tricapic/src/main/java/com/example/demo/controller/UserController.java
+++ tricapic/src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,9 @@ import com.example.demo.data_tables.Capsules;
 import com.example.demo.data_tables.Users;
 import com.example.demo.service.UserService;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+
+
 
 @RestController
 public class UserController {
@@ -35,10 +42,31 @@ public CapsuleUser getUserWithCapsules(@PathVariable String userId) {
     capsuleUser.setName(user.getName());
 
     List<String> capsuleIds = capsules.stream().map(capsule -> String.valueOf(capsule.getCapsulesId())).collect(Collectors.toList());
-    
+
     capsuleUser.setCapsulesId(capsuleIds);
+    capsuleUser.setCapsulesCount(capsules.size());
+
+    //iconImageはパス名のため、base64でエンコード、デコードを行う。
+    String imageDataBase64 = encodeImageToBase64(user.getIconImage());
+    capsuleUser.setImageDataBase64(imageDataBase64);
+
+    //フレンドの有無、フレンドの数（舘山くん作プッシュを利用していく）
 
     return capsuleUser;
+}
+
+// 画像をBase64にエンコードするメソッド
+private String encodeImageToBase64(String imagePath) {
+    try {
+        // 画像ファイルをバイト配列として読み込む
+        byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
+
+        // バイト配列をBase64にエンコードして文字列として返す
+        return Base64.encodeBase64String(imageBytes);
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }
 }
 
 
@@ -49,6 +77,8 @@ public CapsuleUser getUserWithCapsules(@PathVariable String userId) {
         private String profile;
         private String name;
         private List<String> capsulesId;
+        private int capsulesCount;
+        private String imageDataBase64;
 
             // iconImageのゲッター
         public String getIconImage() {
@@ -89,5 +119,24 @@ public CapsuleUser getUserWithCapsules(@PathVariable String userId) {
         public void setCapsulesId(List<String> capsulesId) {
             this.capsulesId = capsulesId;
         }
+
+        // capsulesCountのゲッター
+        public int getCapsulesCount() {
+            return capsulesCount;
+        }
+
+        // capsulesCountのセッター
+        public void setCapsulesCount(int capsulesCount) {
+            this.capsulesCount = capsulesCount;
+        }
+
+        // imageDataBase64のゲッター
+        public String getImageDataBase64() {
+            return imageDataBase64;
+        }
+
+        // imageDataBase64のセッター
+        public void setImageDataBase64(String imageDataBase64) {
+            this.imageDataBase64 = imageDataBase64;
     }
 }
